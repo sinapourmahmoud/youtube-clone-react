cdfdacb0c74edbccf4e0d870bdea16c76826d465
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-03-18 21:08:27 +0330

    adding context to project

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import SearchFeed from "./pages/SearchFeed";
 import VideoDetail from "./pages/VideoDetail";
 import { Routes, Route } from "react-router-dom";
 
+import { mainContext } from "./contexts/mainContext";
 import Header from "./components/Header";
 function App() {
   return (
-    <div className="bg-black">
-      <Header />
+    <mainContext.Provider value={{}}>
+      <div className="bg-black">
+        <Header />
 
-      <div>
-        <Routes>
-          <Route element={<Feed />} path="/" />
-          <Route element={<SearchFeed />} path="/search/:id" />
-          <Route element={<VideoDetail />} path="/video/:id" />
-          <Route element={<ChannelDetail />} path="/channel/:id" />
-        </Routes>
+        <div>
+          <Routes>
+            <Route element={<Feed />} path="/" />
+            <Route element={<SearchFeed />} path="/search/:id" />
+            <Route element={<VideoDetail />} path="/video/:id" />
+            <Route element={<ChannelDetail />} path="/channel/:id" />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </mainContext.Provider>
   );
 }
 
