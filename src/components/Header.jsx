f27f1ef3f054daa1e1d35fc136a53de61a8b7cb9
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-03-18 19:06:35 +0330

    finish header frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { logo } from "./../utils/constants";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <div className=""></div>;
+  let [search, setSearch] = useState("");
+  return (
+    <div className="bg-black px-4 py-1  flex items-center  justify-between">
+      <img src={logo} alt="logo" className="w-[50px] object-contain mt-2" />
+      <form>
+        <div className="flex items-center bg-white rounded-3xl">
+          <input
+            type="text"
+            className=" outline-none border-0 h-full p-3 rounded-3xl"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <Link to={`/search/${search}`}>
+            <SearchIcon sx={{ cursor: "pointer", marginRight: 1 }} />
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
