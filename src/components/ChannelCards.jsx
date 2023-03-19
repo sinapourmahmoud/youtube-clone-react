2cffbc487f07515afec49ca481c336f9236d5c6a
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-03-19 11:37:34 +0330

    finish

--- src/components/ChannelCards.jsx
+++ src/components/ChannelCards.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   demoChannelTitle,
   demoChannelUrl,
   demoProfilePicture,
 } from "../utils/constants";
+
 export default function ChannelCards({ url, title, channelId, styles }) {
+  let imageRef = useEffect(null);
+
   return (
     <Link
       to={`/channel/${channelId || demoChannelUrl}`}
       className={`w-[250px] h-[250px] flex items-center justify-center gap-2 flex-col ${styles}`}
     >
       <img
+        ref={imageRef}
         src={url || demoProfilePicture}
         alt="channel"
         className="w-[180px] h-[180px] rounded-full object-cover"
